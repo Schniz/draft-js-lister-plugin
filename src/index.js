ce72df5174b3daedefc296feb16cc6d154fe95c3
Gal Schlezinger <galstar@spitfire.co.il>
2016-05-30 11:34:25 +0300

    retabbed

--- src/index.js
+++ src/index.js
@@ -10,56 +10,56 @@
 import { EditorState, RichUtils, SelectionState, Modifier } from 'draft-js';
 import type { PluginsFns } from './utils';
 import {
-	getKeyAndOffset,
-	extractListsInBlock,
-	isSignOfUnorderedList,
-	isSignOfOrderedList,
+  getKeyAndOffset,
+  extractListsInBlock,
+  isSignOfUnorderedList,
+  isSignOfOrderedList,
 } from './utils';
-import extractListInBlock from './extractListInBlock';
 import removeList from './removeList';
 
 export default () => {
 
+  return {
 
-	return {
+    handleReturn(e : any, callbacks : PluginsFns) {
+      return removeList(callbacks);
+    },
 
-		handleReturn(e : any, callbacks : PluginsFns) {
-			return removeList(callbacks);
-		},
+    handleBeforeInput(typedChar: string, { getEditorState, setEditorState } : PluginsFns) {
+      if (typedChar !== ' ') return false;
 
-		handleBeforeInput(typedChar: string, { getEditorState, setEditorState } : PluginsFns) {
-			if (typedChar !== ' ') return false;
+      const editorState = getEditorState();
+      const { key } = getKeyAndOffset(editorState);
+      const newState = extractListsInBlock(' ', editorState, key);
 
-			const editorState = getEditorState();
-			const { key } = getKeyAndOffset(editorState);
-			const newState = extractListsInBlock(' ', editorState, key);
+      if (newState) {
+        setEditorState(newState);
+        return true;
+      }
 
-			if (newState) {
-				setEditorState(newState);
-				return true;
-			}
+      return false;
+    },
 
-			return false;
-		},
+    handleKeyCommand(command : string, callbacks : PluginsFns) {
+      if (command !== 'backspace') return;
+      return removeList(callbacks);
+    },
 
-		handleKeyCommand(command : string, callbacks : PluginsFns) {
-			if (command !== 'backspace') return;
-			return removeList(callbacks);
-		},
+    handlePastedText(text : any, html : any, callbacks : PluginsFns) {
+      // We have to setTimeout so it would run after the current iteration
+      // since we might have pasted a lot of text from word, etc.
+      setTimeout(() => {
+        const editorState = callbacks.getEditorState();
+        const currentContent = editorState.getCurrentContent();
+        const blockMap = currentContent.getBlockMap();
+        const newState = blockMap.valueSeq().reduce((acc, currentBlock) => {
+          return extractListsInBlock('', acc, currentBlock.key) || acc;
+        }, editorState);
 
-		handlePastedText(text : any, html : any, callbacks : PluginsFns) {
-			setTimeout(() => {
-				const editorState = callbacks.getEditorState();
-				const currentContent = editorState.getCurrentContent();
-				const blockMap = currentContent.getBlockMap();
-				const newState = blockMap.valueSeq().reduce((acc, currentBlock) => {
-					return extractListsInBlock('', acc, currentBlock.key) || acc;
-				}, editorState);
+        callbacks.setEditorState(newState);
+      });
+    },
 
-				callbacks.setEditorState(newState);
-			});
-		},
-
-	};
+  };
 
 };
--- src/removeList.js
+++ src/removeList.js
@@ -5,19 +5,18 @@ import type { PluginsFns } from './utils';
 import { getKeyAndOffset } from './utils';
 
 export default function removeList({ getEditorState, setEditorState } : PluginsFns) {
-	const editorState = getEditorState();
-	const { key, offset } = getKeyAndOffset(editorState);
-	const block = editorState.getCurrentContent().getBlockMap().get(key);
-	if (offset !== 0 || ['ordered-list-item', 'unordered-list-item'].indexOf(block.getType()) === -1) {
-		return;
-	}
+  const editorState = getEditorState();
+  const { key, offset } = getKeyAndOffset(editorState);
+  const block = editorState.getCurrentContent().getBlockMap().get(key);
+  if (offset !== 0 || ['ordered-list-item', 'unordered-list-item'].indexOf(block.getType()) === -1) {
+    return;
+  }
 
-	const newState = RichUtils.toggleBlockType(
-		editorState,
-		block.getType(),
-	);
+  const newState = RichUtils.toggleBlockType(
+    editorState,
+    block.getType(),
+  );
 
-	setEditorState(newState);
-	return true;
+  setEditorState(newState);
+  return true;
 }
-
